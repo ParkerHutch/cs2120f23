88119d6681e54fb050873526c5227a100e4290ab
Parker Hutchinson <parker.hutch@outlook.com>
2024-04-10 12:11:25 -0400

    add lecture notes

---
+++ Student/lecture_04_10.lean
@@ -0,0 +1,14 @@
+-- properties
+-- (refl r): reflexive
+-- symm
+-- trans
+-- equiv
+-- partial order
+
+-- congruent b n mod := n % b = m % b
+-- prove that congruence is an equivalence relation
+
+-- properties of functions
+-- many-to-one function
+-- one-to-one injections
+-- injection function
