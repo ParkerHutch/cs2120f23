2d7d2689990d9b40d021afb2975b227dca91d51a
Parker Hutchinson <parker.hutch@outlook.com>
2024-04-15 11:05:04 -0400

    add lecture notes

--- Student/lecture_04_10.lean
+++ Student/lecture_04_10.lean
@@ -12,3 +12,7 @@
 -- many-to-one function
 -- one-to-one injections
 -- injection function
+
+-- injective r
+-- ∀ abc, r a c → r b c (then) → a = b
+-- single valued property and injective property are kind of like inverses
